6a3668ff9736614174c2b1ea7af1ee626ba54c83
Rakshitha Kannan <Rakshithakannan07@gmail.com>
2026-02-19 14:38:22 -0500

    i added more hellos

--- Hello.py
+++ Hello.py
@@ -1,16 +1,2 @@
 print("Hello, World!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-output = "Hello, World!"
-print(output)
-print("Hello, World!" * 3)
-=======
-print("bruh")
->>>>>>> da9ef4b (bruh)
-=======
-print("bruh")
-=======
-output = "Hello, World!"
-print(output)
->>>>>>> 4379263 (HelloWorld change)
->>>>>>> e6f515b (HelloWorld change)
+print("Welcome to HackHers Spring 26!")
