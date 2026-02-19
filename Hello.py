a33ffcdcb2fd778e7b80fcd681f336e5b3c671b7
Rakshitha Kannan <Rakshithakannan07@gmail.com>
2026-02-19 14:24:28 -0500

    HelloWorld change

--- Hello.py
+++ Hello.py
@@ -1,8 +1,16 @@
 print("Hello, World!")
 <<<<<<< HEAD
+<<<<<<< HEAD
 output = "Hello, World!"
 print(output)
 print("Hello, World!" * 3)
 =======
 print("bruh")
 >>>>>>> da9ef4b (bruh)
+=======
+print("bruh")
+=======
+output = "Hello, World!"
+print(output)
+>>>>>>> 4379263 (HelloWorld change)
+>>>>>>> e6f515b (HelloWorld change)
